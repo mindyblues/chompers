f6654ccfa5757187704c494dcfd3298c5e64008d
mindyblues <mvmann@crimson.ua.edu>
2017-01-29 21:50:23 +0100

    lol

--- src/todolist.py
+++ src/todolist.py
@@ -1,6 +1,7 @@
 # to do list
 
-3. babies should be 60% likely to catch the mutation of their Parent if such exists
+3. babies should be 60% likely to catch the mutation of their Parent if such exists (inheritance system
+for mutations like there is for colors)
 4. define bicolor baby chances (Mindy)
 5. set up day system
 
